1b682af5fb7122dcbeafee7b8210221a0559787e
Elmira Aliyeva <ealiyeva17624@ada.edu.az>
2026-02-01 18:15:48 +0400

    Update App greeting messsage

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,8 +5,9 @@ package org.example;
 
 public class App {
     public String getGreeting() {
-        return "Gradle build and Git workflow successful.";
+        return "Updated greeting from Student B (feature branch).";
     }
+    
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
